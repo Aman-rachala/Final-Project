f54cb0ef9bdc14379af1968521ee1d0c11970dad
unknown <amanrachala@gmail.com>
2024-04-14 17:49:16 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,178 @@
+import os
+import sys
+import numpy as np
+import torch
+from flask import Flask, request, jsonify, render_template, Response
+from utilities import transform, filter_bboxes_from_outputs, get_image_region, plot_results
+from transformers import (
+    DetrForObjectDetection,
+    DPTForDepthEstimation,
+    DPTFeatureExtractor,
+    VisionEncoderDecoderModel,
+    ViTImageProcessor,
+    AutoTokenizer,
+)
+from PIL import Image
+
+# Initializing a vision encoder-decoder model for image captioning
+ve_model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+# Initializing a feature extractor for the Vision Transformer (ViT)
+# This will be used to process images and extract relevant features
+feature_extractor_vit = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+# Initializing a tokenizer for text input
+# This will convert raw text into tokenized input that the model can understand
+tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+
+app = Flask(__name__)
+detrmodel = DetrForObjectDetection.from_pretrained("facebook/detr-resnet-50")
+dptmodel = DPTForDepthEstimation.from_pretrained("Intel/dpt-large")
+feature_extractor = DPTFeatureExtractor.from_pretrained("Intel/dpt-large")
+detrmodel.eval()
+dptmodel.eval()
+
+
+# Home route
+@app.route("/")
+def home():
+    return render_template("index2.html")
+
+
+@app.route("/upload")
+def photo():
+    return render_template("index.html")
+
+
+@app.route("/predict", methods=["POST"])
+def predict(priority_list=[]):
+    # priority_list = []
+    file = request.files["data"]
+    print("No.of files: ", len(request.files))
+    print("priority_list: ",priority_list)
+    # if os.path.exists("static/uploaded_image.jpg"):
+    #     os.remove("static/uploaded_image.jpg")  # Delete the file
+    #     print("Deletion successful!")
+    file.save("static/uploaded_image.jpg")
+    # Read the image via file.stream
+    im = Image.open(file.stream)
+    img = transform(im).unsqueeze(0)
+
+    height, width = img.shape[-2], img.shape[-1]
+    print("Height:", height)
+    print("Width:", width)
+    print("Image: ", img)
+
+    outputs = detrmodel(img) 
+    print("Outputs: ", outputs)   
+    probas_to_keep, bboxes_scaled, final_outputs = filter_bboxes_from_outputs(im, outputs, threshold=0.95)
+    plot_results(im, probas_to_keep, bboxes_scaled)
+    print('final_outputs: ',final_outputs)
+    
+    object_list = [[int(item) if isinstance(item, float) else item for item in sublist] for sublist in final_outputs]
+    final_outputs = []
+    print('final_outputs: ',final_outputs)
+    print("Objects_list: ", object_list)
+
+
+    pixel_values = feature_extractor(im, return_tensors="pt").pixel_values
+    with torch.no_grad():
+        outputs = dptmodel(pixel_values)
+        predicted_depth = outputs.predicted_depth
+    prediction = torch.nn.functional.interpolate(
+        predicted_depth.unsqueeze(1),
+        size=im.size[::-1],
+        mode="bicubic",
+        align_corners=False,
+    ).squeeze()
+    output = prediction.cpu().numpy()
+    formatted = (output * 255 / np.max(output)).astype("uint8")
+    # cv2.imread('/content/Depth Image', cv2.IMREAD_GRAYSCALE)
+    depth_image = formatted
+    # Perform object detection or segmentation on the normal image to obtain object bounding boxes and names
+    # and store them in a list called 'objects' with elements of the form: (object_name, x, y, w, h)
+    objects = object_list
+    print("Objects: ",objects)
+    # Calculate average depth value for each object
+    
+    objects_with_depth = []
+    print("objects with depth: ",objects_with_depth)
+    for idx, obj in enumerate(objects):
+        (
+            object_name,
+            x,
+            y,
+            w,
+            h,
+            origin,
+        ) = obj  # Object bounding box coordinates and name
+        depth_values = depth_image[y : y + h, x : x + w]
+        average_depth = np.mean(depth_values)
+        objects_with_depth.append(([object_name, idx, origin], average_depth))
+
+    # Sort objects based on depth (lower depth values have higher priority)
+    objects_with_depth.sort(key=lambda x: x[1])
+    print("objects with depth: ",objects_with_depth)
+    # Create a priority list of objects with object names and initial indices
+    priority_list = [obj[0] for obj in objects_with_depth]
+    print("Priority list:",priority_list)
+    priority_list = sorted(priority_list, key=lambda x: x[1])
+    print("Priority list:",priority_list)
+    print("-" * 20)
+    print(f"The priority list is: \n{priority_list}")
+    print("-" * 20)
+
+    regions = {
+        "Top-Left": (-width // 3, height // 3, -width // 6, height // 2),
+        "Top-Center": (-width // 6, height // 3, width // 6, height // 2),
+        "Top-Right": (width // 6, height // 3, width // 3, height // 2),
+        "Middle-Left": (-width // 3, -height // 6, -width // 6, height // 3),
+        "Middle-Center": (-width // 6, -height // 6, width // 6, height // 3),
+        "Middle-Right": (width // 6, -height // 6, width // 3, height // 3),
+        "Bottom-Left": (-width // 3, -height // 3, -width // 6, -height // 6),
+        "Bottom-Center": (-width // 6, -height // 3, width // 6, -height // 6),
+        "Bottom-Right": (width // 6, -height // 3, width // 3, -height // 6),
+    }
+
+
+    for i in priority_list:
+        center = i[2]
+        i.insert(3, get_image_region(center, height, width, regions))
+
+    
+
+    grp = {}
+    for k in regions.keys():
+        grp[k] = []
+    l = len(priority_list)
+    delim = l if l < 5 else 5
+    for i in priority_list[:delim]:
+        grp[i[3]].append(i[0])
+
+    answer = "The most Priority objects are :"
+    for k, v in grp.items():
+        text = ""
+        if len(v):
+            for i in v:
+                text += i
+                text += " "
+            text += " at " + k
+            answer += text + ", "
+    print(answer)
+
+    # if os.path.exists("static/uploaded_image.jpg"):
+    #     os.remove("static/uploaded_image.jpg")  # Delete the file
+    #     print("Deletion successful!")
+    # if os.path.exists("static/images/image.jpg"):
+    #     os.remove("static/images/image.jpg")  # Delete the file
+    #     print("Deletion successful!")
+        # print(f"File {static/uploaded_image.jpg} deleted successfully.")
+
+    return Response(answer)
+
+@app.route('/restart', methods=['POST'])
+def restart_server():
+    # Optional: Perform any cleanup or save data before restarting
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
+app.run(debug=True)
---
+++ utilities.py
@@ -0,0 +1,255 @@
+import torch, torchvision
+
+import torchvision.transforms as T
+
+# standard PyTorch mean-std input image normalization
+transform = T.Compose(
+    [
+        T.Resize(800),
+        T.ToTensor(),
+        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]
+)
+
+
+def box_cxcywh_to_xyxy(x):
+    x_c, y_c, w, h = x.unbind(1)
+    b = [(x_c - 0.5 * w), (y_c - 0.5 * h), (x_c + 0.5 * w), (y_c + 0.5 * h)]
+    return torch.stack(b, dim=1)
+
+
+def rescale_bboxes(out_bbox, size):
+    img_w, img_h = size
+    b = box_cxcywh_to_xyxy(out_bbox)
+    b = b * torch.tensor([img_w, img_h, img_w, img_h], dtype=torch.float32)
+    return b
+
+
+test = []
+
+
+def filter_bboxes_from_outputs(im, outputs, threshold=0.7):
+    # keep only predictions with confidence above threshold
+    probas = outputs["logits"].softmax(-1)[0, :, :-1]
+    keep = probas.max(-1).values > threshold
+
+    probas_to_keep = probas[keep]
+
+    # convert boxes from [0; 1] to image scales
+    bboxes_scaled = rescale_bboxes(outputs["pred_boxes"][0, keep], im.size)
+    print(bboxes_scaled[0])
+    for p, (xmin, ymin, xmax, ymax) in zip(probas_to_keep, bboxes_scaled.tolist()):
+        cl = p.argmax()
+        test.append(
+            [
+                CLASSES[cl],
+                xmin,
+                ymin,
+                xmax - xmin,
+                ymax - ymin,
+                change_origin(im, xmin, xmax, ymin, ymax),
+            ]
+        )
+    print(test)
+    return probas_to_keep, bboxes_scaled, test
+
+
+CLASSES = [
+    "N/A",
+    "person",
+    "bicycle",
+    "car",
+    "motorcycle",
+    "airplane",
+    "bus",
+    "train",
+    "truck",
+    "boat",
+    "traffic light",
+    "fire hydrant",
+    "N/A",
+    "stop sign",
+    "parking meter",
+    "bench",
+    "bird",
+    "cat",
+    "dog",
+    "horse",
+    "sheep",
+    "cow",
+    "elephant",
+    "bear",
+    "zebra",
+    "giraffe",
+    "N/A",
+    "backpack",
+    "umbrella",
+    "N/A",
+    "N/A",
+    "handbag",
+    "tie",
+    "suitcase",
+    "frisbee",
+    "skis",
+    "snowboard",
+    "sports ball",
+    "kite",
+    "baseball bat",
+    "baseball glove",
+    "skateboard",
+    "surfboard",
+    "tennis racket",
+    "bottle",
+    "N/A",
+    "wine glass",
+    "cup",
+    "fork",
+    "knife",
+    "spoon",
+    "bowl",
+    "banana",
+    "apple",
+    "sandwich",
+    "orange",
+    "broccoli",
+    "carrot",
+    "hot dog",
+    "pizza",
+    "donut",
+    "cake",
+    "chair",
+    "couch",
+    "potted plant",
+    "bed",
+    "N/A",
+    "dining table",
+    "N/A",
+    "N/A",
+    "toilet",
+    "N/A",
+    "tv",
+    "laptop",
+    "mouse",
+    "remote",
+    "keyboard",
+    "cell phone",
+    "microwave",
+    "oven",
+    "toaster",
+    "sink",
+    "refrigerator",
+    "N/A",
+    "book",
+    "clock",
+    "vase",
+    "scissors",
+    "teddy bear",
+    "hair drier",
+    "toothbrush",
+]
+
+# colors for visualization
+COLORS = [
+    [0.000, 0.447, 0.741],
+    [0.850, 0.325, 0.098],
+    [0.929, 0.694, 0.125],
+    [0.494, 0.184, 0.556],
+    [0.466, 0.674, 0.188],
+    [0.301, 0.745, 0.933],
+]
+
+
+import matplotlib.pyplot as plt
+
+
+def change_origin(im, xmin, xmax, ymin, ymax):
+    width, height = im.size
+    print("Width, ht: ", width, height)
+    origin_x = width // 2
+    origin_y = -height // 2
+
+    center_x = int((xmax + xmin) // 2)
+    center_y = int(-(ymax + ymin) // 2)
+
+    center_x = center_x - origin_x
+    center_y = center_y - origin_y
+
+    return [center_x, center_y]
+
+
+test_origin = []
+
+
+def plot_results(pil_img, prob=None, boxes=None):
+    plt.figure(figsize=(12, 9))
+    plt.imshow(pil_img)
+    ax = plt.gca()
+    colors = COLORS * 100
+    if prob is not None and boxes is not None:
+        for p, (xmin, ymin, xmax, ymax), c in zip(prob, boxes.tolist(), colors):
+            ax.add_patch(
+                plt.Rectangle(
+                    (xmin, ymin),
+                    xmax - xmin,
+                    ymax - ymin,
+                    fill=False,
+                    color=c,
+                    linewidth=3,
+                )
+            )
+            cl = p.argmax()
+            text = f"{CLASSES[cl]}: {p[cl]:0.2f}"
+            ax.text(xmin, ymin, text, fontsize=15, bbox=dict(facecolor="yellow", alpha=0.5))
+    plt.axis("off")
+    plt.savefig("./static/images/image.jpg")
+
+
+# regions = {
+#     "Top-Left": (-800, 400, -400, 800),
+#     "Top-Center": (-400, 400, 400, 800),
+#     "Top-Right": (400, 400, 800, 800),
+#     "Middle-Left": (-800, -400, -400, 400),
+#     "Middle-Center": (-400, -400, 400, 400),
+#     "Middle-Right": (400, -400, 800, 400),
+#     "Bottom-Left": (-800, -800, 400, -400),
+#     "Bottom-Center": (-400, -800, 400, -400),
+#     "Bottom-Right": (400, -800, 800, -400),
+# }
+
+
+
+
+# def get_image_region(point):
+#     x, y = point  # Extract x and y coordinates from the point
+
+#     # Define the dimensions and boundaries of each region
+
+#     # Iterate through the regions and check if the point falls within any of them
+#     for region, (x1, y1, x2, y2) in regions.items():
+#         if x1 <= x <= x2 and y1 <= y <= y2:
+#             return region  # Return the region if the point falls within it
+
+#     return "Unknown"  # Return 'Unknown' if the point is outside all regions or if the input is invalid
+
+def get_image_region(point, height, width, regions):
+    x, y = point  # Extract x and y coordinates from the point
+
+    # Define the dimensions and boundaries of each region
+    # regions = {
+    #     "Top-Left": (0, height // 2, width // 2, height),
+    #     "Top-Center": (width // 4, height // 2, 3 * width // 4, height),
+    #     "Top-Right": (width // 2, height // 2, width, height),
+    #     "Middle-Left": (0, height // 4, width // 2, 3 * height // 4),
+    #     "Middle-Center": (width // 4, height // 4, 3 * width // 4, 3 * height // 4),
+    #     "Middle-Right": (width // 2, height // 4, width, 3 * height // 4),
+    #     "Bottom-Left": (0, 0, width // 2, height // 2),
+    #     "Bottom-Center": (width // 4, 0, 3 * width // 4, height // 2),
+    #     "Bottom-Right": (width // 2, 0, width, height // 2),
+    # }
+
+    # Iterate through the regions and check if the point falls within any of them
+    for region, (x1, y1, x2, y2) in regions.items():
+        if x1 <= x <= x2 and y1 <= y <= y2:
+            return region  # Return the region if the point falls within it
+
+    return "Unknown" 
